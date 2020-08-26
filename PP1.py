d895609c2004e380287b2f44ff62e52999fb1de3
Dannie Chen <dannie.chen.bj@gmail.com>
2020-08-26 01:47:53 -0500

    w/o Kwords timezone update

--- PP1.py
+++ PP1.py
@@ -8,11 +8,6 @@ pytrends.build_payload(kw_list=['foundation'], geo = 'US', timeframe = 'today 1-
 related_queries= pytrends.related_queries()
 print(related_queries)
 
-startTime = time.time()
-pytrends.build_payload(kw_list=['foundation'], geo = 'US', timeframe = "2019-01-01 2020-07-28", cat = 0)
-related_queries= pytrend.related_queries()
-print(related_queries)
-
 pytrends = TrendReq(hl='en-US', tz=360, timeout=(10, 25))
 trends = pytrends.related_queries()
 df = pd.read_csv("keyword_list_1.csv")
@@ -24,7 +19,7 @@ dataset = []
 for x in range(0, len(df2)):
     print(df2[x])
     trends_value = trends[df2[x]]['top'].value.tolist().head(50)
-    pytrends.build_payload(kw_list=trends_keywords, cat=0, timeframe='2019-01-01 2020-07-28', geo='US')
+    pytrends.build_payload(kw_list=trends_keywords, cat=0, timeframe='2019-01-01 2020-08-25', geo='US')
     data = pytrends.interest_over_time()
     if not data.empty:
         data = data.drop(labels=['isPartial'], axis='columns')
