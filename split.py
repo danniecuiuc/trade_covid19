49d6ed99e20717a1b92a8a1e37b603012a832920
Dannie Chen <dannie.chen.bj@gmail.com>
2020-08-30 01:12:39 -0600

    0-241comps checked, error reported

---
+++ split.py
@@ -0,0 +1,30 @@
+from pytrends.request import TrendReq
+import pandas as pd
+import time
+import pytrends
+import sys
+
+startTime = time.time()
+pytrends = TrendReq(hl='en-US', tz=360, timeout=(10, 25))
+
+trends = pytrends.related_queries()
+
+df_split = pd.read_csv("~/downloads/ra_paper/keyword_list_2.csv")
+df_split_test = df_split["keywords"].values.tolist()[231:241]   #0-241 DONE
+
+for x in range(0, len(df_split_test)): 
+    keywords = [df_split_test[x]]
+    print(keywords)
+
+print(df_split_test)
+
+keyword_data = pd.DataFrame()
+
+for x in range(0, len(df_split_test)): 
+    startTime = time.time()
+    pytrends.build_payload(kw_list=[df_split_test[x]], cat=0, timeframe='2020-01-01 2020-07-30', geo='US')
+    related_queries = pytrends.related_queries()
+    trends_value = related_queries[df_split_test[x]]['top'].head(25)
+    keyword_data = keyword_data.append(trends_value)
+
+print(keyword_data)
