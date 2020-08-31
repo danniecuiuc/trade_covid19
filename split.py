a9a91283d23d1b56ab5bc30b1684b42033278c88
Dannie Chen <dannie.chen.bj@gmail.com>
2020-08-31 01:16:14 -0600

    0-300 checked

--- split.py
+++ split.py
@@ -7,7 +7,7 @@ startTime = time.time()
 pytrends = TrendReq(hl='en-US', tz=360, timeout=(10, 25))
 
 df_split = pd.read_csv("~/downloads/ra_paper/keyword_list_2.csv",encoding = "ISO-8859-1")
-df_split_test = df_split["keywords"].values.tolist()[261:271]   #0-261 DONE
+df_split_test = df_split["keywords"].values.tolist()[300:311]   #0-301 DONE
 
 for x in range(0, len(df_split_test)): 
     keywords = [df_split_test[x]]
