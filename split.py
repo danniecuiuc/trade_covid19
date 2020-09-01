ca0023dcedcbc35650a7eb200faccdbe9eb1ecb9
Dannie Chen <dannie.chen.bj@gmail.com>
2020-09-01 01:20:26 -0600

    add find none values

--- split.py
+++ split.py
@@ -7,7 +7,7 @@ startTime = time.time()
 pytrends = TrendReq(hl='en-US', tz=360, timeout=(10, 25))
 
 df_split = pd.read_csv("~/downloads/ra_paper/keyword_list_2.csv",encoding = "ISO-8859-1")
-df_split_test = df_split["keywords"].values.tolist()[300:311]   #0-301 DONE
+df_split_test = df_split["keywords"].values.tolist()  #0-301 DONE
 
 for x in range(0, len(df_split_test)): 
     keywords = [df_split_test[x]]
@@ -19,7 +19,15 @@ for x in range(0, len(df_split_test)):
     startTime = time.time()
     pytrends.build_payload(kw_list=[df_split_test[x]], cat=0, timeframe='2020-01-01 2020-07-30', geo='US')
     related_queries = pytrends.related_queries()
-    trends_value = related_queries[df_split_test[x]]['top'].head(25)
-    keyword_data = keyword_data.append(trends_value)
+    trends_value = related_queries[df_split_test[x]]['top']
+    non_value = len(trends_value)==0
+    print(non_value)
+
+#  for x in range(0, len(df_split_test)): 
+    #  startTime = time.time()
+    #  pytrends.build_payload(kw_list=[df_split_test[x]], cat=0, timeframe='2020-01-01 2020-07-30', geo='US')
+    #  related_queries = pytrends.related_queries()
+    #  trends_value = related_queries[df_split_test[x]]['top'].head(25)
+    #  keyword_data = keyword_data.append(trends_value)
 
 print(keyword_data)
